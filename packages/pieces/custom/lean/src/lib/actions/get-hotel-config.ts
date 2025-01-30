cde61ab7a66262e9b839047bd41bb19248da92b2
Rajat <rajat.arora@clicsoft.io>
2025-01-31 01:09:36 +0530

    Get cif & tax number

--- packages/pieces/custom/lean/src/lib/actions/get-hotel-config.ts
+++ packages/pieces/custom/lean/src/lib/actions/get-hotel-config.ts
@@ -1,7 +1,7 @@
 import { createAction, DynamicPropsValue, Property } from '@activepieces/pieces-framework';
-import { createHttpPostRequest, decode } from '../common/commonFunctions';
 import { httpClient, HttpHeaders, HttpMethod } from '@activepieces/pieces-common';
-import { TAXES_KEYS_MAPPING } from '../common/constants';
+import { TAXES_KEYS_MAPPING } from '../../common/constants';
+import { createHttpPostRequest, decode } from '../../common/commonFunctions';
 
 interface Tax {
   id: number;
@@ -123,25 +123,41 @@ export const getHotelConfig = createAction({
           const token = login?.body?.token;
           console.log("lean token response:::::", token, data.hotelId, typeof data.hotelId);
           // return {token, id: data.hotelId};
-           const headers: HttpHeaders = {
-             Authorization: `Token ${token}`,
-           };
+          const headers: HttpHeaders = {
+              Authorization: `Token ${token}`,
+          };
           const hotelEndPoint = `${data.url}/api/v2/hotels?id=${Number(data.hotelId)}`
           const taxesEndPoint = `${data.url}/api/v2/taxes/`;
+          const companiesEndpoint = `${data.url}/api/v2/customers/companies`;
           const hotelRequest = createHttpPostRequest(
-             'GET' as HttpMethod,
-             hotelEndPoint,
-             headers,
-             {}
-           );
-           const taxesRequest = createHttpPostRequest(
-             'GET' as HttpMethod,
-             taxesEndPoint,
-             headers,
-             {}
-           );
+            'GET' as HttpMethod,
+            hotelEndPoint,
+            headers,
+            {}
+          );
+          const taxesRequest = createHttpPostRequest(
+            'GET' as HttpMethod,
+            taxesEndPoint,
+            headers,
+            {}
+          );
           const hotelRes = await httpClient.sendRequest(hotelRequest);
           const hotelInfo = hotelRes.body[0];
+          console.log("hotel info", JSON.stringify(hotelInfo));
+          const companyCode = hotelInfo?.chain_company_code || "";
+          const companyRequest: any = createHttpPostRequest(
+            'GET' as HttpMethod,
+            companiesEndpoint,
+            headers,
+            {}
+          );          
+          const companyRes = await httpClient.sendRequest(companyRequest);
+          console.log("companies request", JSON.stringify(companyRes));
+          const currentCompany = companyRes?.status === 200 ? companyRes?.body?.results?.find((company: any)=> 
+            company?.id === companyCode || company?.name === companyCode || company?.cif === companyCode) : {};
+
+          const cif = currentCompany?.cif ?? "", taxNumber = (currentCompany?.tax_id || currentCompany?.tax2_id) ?? "";
+        
           const taxesRes = await httpClient.sendRequest(taxesRequest);
           const taxes = taxesRes.body.map((tax: any)=> {
             return Object.keys(TAXES_KEYS_MAPPING).reduce(
@@ -169,6 +185,8 @@ export const getHotelConfig = createAction({
                 country: hotelInfo.country || '',
                 email: hotelInfo.email || '',
                 phone: hotelInfo.phone || '',
+                cif: cif,
+                taxNumber: taxNumber
             },
             additionalInfo: {
               taxes,
