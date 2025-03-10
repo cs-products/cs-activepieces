e57d762cb041bbe36edbdc7dc31cb15af9c22c28
Mayank Bhargava <mayank.bhargava@clicsoft.io>
2025-03-10 14:26:19 +0530

    chore: gimmy piece bug fixed

--- packages/pieces/custom/gimmy/src/lib/actions/generate-gimmy-data.ts
+++ packages/pieces/custom/gimmy/src/lib/actions/generate-gimmy-data.ts
@@ -238,7 +238,7 @@ export const generateGimmyData = createAction({
               const type = orderItem?.type;
 
               const salesObject = {
-                pms_id: `${gimmyObj.pms_id}-${orderItem?.productCode}`,
+                pms_id: `${gimmyObj.pms_id}-${orderItem?.productCode} may`,
                 type: type === 'HOTEL_RESERVATION' ? 'ACCOMODATION' : 'EXTRA',
                 label: `${orderName}`,
                 quantity: orderItem?.count || 1,
